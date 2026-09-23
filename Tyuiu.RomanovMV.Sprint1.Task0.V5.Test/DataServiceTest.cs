ab7a2e09aeb9f10c305c8035d398d1fbe45c3eed
TyuiuRomanovMV <max2002romanov15@gmail.com>
2026-09-23 21:48:00 +0500

    Add project files.

---
+++ Tyuiu.RomanovMV.Sprint1.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿
+using Tyuiu.RomanovMV.Sprint1.Task0.V5.Lib;
+
+namespace Tyuiu.RomanovMV.Sprint1.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(12, res);
+        }
+    }
+}
